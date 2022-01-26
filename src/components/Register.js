58c2f33705c3f9a719e3dc835fb43abbb9fa2d10
Evgenia-N <jazzpenguin@yandex.ru>
2022-01-26 23:28:18 +0300

    feat: разметка Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import * as auth from "../auth";
 import React from "react";
+import "./styles/Register.css"
 
 export default function Register() {
   const [values, setValues] = React.useState({
@@ -29,5 +30,42 @@ export default function Register() {
         navigate('/sign-in')
       }})
     }
-  return
+
+    return (
+      <div className="register">
+        <p className="register__enter">Регистрация</p>
+        <form onSubmit={handleSubmit} className="register__form">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+            className="register__input"
+            placeholder="Email"
+          />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+            className="register__input"
+            placeholder="Пароль"
+          />
+            <button
+              type="submit"
+              onSubmit={handleSubmit}
+              className="register__link">
+            </button>
+        </form>
+  
+        <div className="register-login">
+          <p className="register-login__text">Уже зарегистрированы?</p>
+          <Link to="/sign-in" className="register-login__link">
+            Войти
+          </Link>
+        </div>
+      </div>
+    );
 }
