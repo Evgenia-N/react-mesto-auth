30ab23502c48127d24b2a3e78cad590a461e852a
Evgenia-N <jazzpenguin@yandex.ru>
2022-02-08 03:30:31 +0300

    feat: adaptive design (without the button)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,10 @@ import logo from '../images/logo.svg';
 
 export default function Header({onSignOut, userData}) {
   let {email} = userData;
+
+  function handleMenuClick() {
+    
+  }
   return (
     <header className="header page__header">
     <img src={logo} alt="Логотип_Mesto" className="header__logo" />
@@ -11,11 +15,14 @@ export default function Header({onSignOut, userData}) {
       <Route path='/sign-up' element={
         <Link to='/sign-in' className="header__link">Войти</Link>} /> 
       <Route path='/sign-in' element={
-        <Link to='/sign-up' className="header__link">Регистрация</Link>}/>
+        <Link to='/sign-up' className="header__link">Регистрация</Link>} />
       <Route path='/' element={
-        <div className="header__container">
-          <span className="header__email">{email}</span>
-          <button className="header__logout-button" onClick={onSignOut}>Выйти</button>
+        <div>
+          <div className="header__container">
+            <span className="header__email">{email}</span>
+            <button className="header__logout-button" onClick={onSignOut}>Выйти</button>
+          </div>
+          <button className="header__menu" onClick={handleMenuClick}></button>
         </div>} /> 
     </Routes>
   </header>
